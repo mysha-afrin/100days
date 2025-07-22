6ff6ec7c797f7d5f2b1356e06751ea49e2a267b8
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-22 06:56:41 +0600

    Create band name generator.py

---
+++ day-1/band name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city = input("What's the name of your city?")
+pet = input("What's the name of your pet?")
+print(f"Your band name could be {city} {pet}!")
