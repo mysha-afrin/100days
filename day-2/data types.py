690c0f38218ad2b7db5d2474bcf759a7b9077af2
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-23 13:15:21 +0600

    Create data types.py

---
+++ day-2/data types.py
@@ -0,0 +1,4 @@
+print(6/2) # using one slash for division python detect it as float division
+print(6//2) # using two slashes for floor division
+print(type("Hello"))  # This will print <class 'str'>
+print(3* 3 + 3 / 3 -3)
