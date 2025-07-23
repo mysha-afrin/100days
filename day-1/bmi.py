9bdd226c191ca11592853fffc1f1d07e318af193
myshaafrin <myshaafrinjeba916@gmail.com.com>
2025-07-23 22:41:11 +0600

    Add new content

---
+++ day-1/bmi.py
@@ -0,0 +1,4 @@
+bmi = 84 / 1.65 ** 2
+print(int(bmi))#int gives interger value
+print(bmi)
+print(round(bmi, 2))#round function allows to specify how many disit a coder want to keep after dot.
---
+++ day-1/len function.py
@@ -0,0 +1,5 @@
+len("Hello")
+print(type("Hello"))
+
+name = input("What's your name?")
+print(f"Number of caracters in your name is {len(name)}")
