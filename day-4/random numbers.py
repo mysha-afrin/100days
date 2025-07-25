d1b217dca11506da5e464fabf53a4a37c753c8d7
myshaafrin <myshaafrinjeba916@gmail.com>
2025-07-25 07:20:04 +0600

    Add new content

--- day-4/random numbers.py
+++ day-4/random numbers.py
@@ -2,3 +2,8 @@ import random
 import my_module
 random_integer = random.randint(1, 10)
 print(random_integer)
+random_number_0_1 = random.random()
+print(random_number_0_1)
+
+random_float = random.uniform(1, 10)
+print(random_float)
