f0ecbeaaec8ac2fd38cbc5a30d3313a6d03ce28f
myshaafrin <myshaafrinjeba916@gmail.com>
2025-07-25 20:35:41 +0600

    Add new content

--- day-4/who will pay.py
+++ day-4/who will pay.py
@@ -1,4 +1,4 @@
 import random
-list = ["Soikot"]# "Jui", "Peal", "Mumu", "Monamee"]
+list = ["Soikot", "jeba", "Sakib", "Shanto", "Rifat", "Sabbir", "Shuvo", "Samiul", "Sabbir Hossain"]
 random_choice = random.choice(list)
 print(f"{random_choice} will pay the bill today!")  # Randomly selects a person to pay the bill
