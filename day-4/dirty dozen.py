d77669b050fdcc05399af995ff3c056afddf6fb0
myshaafrin <myshaafrinjeba916@gmail.com>
2025-07-25 20:33:42 +0600

    Add new content

---
+++ day-4/dirty dozen.py
@@ -0,0 +1,7 @@
+#dirty_dozen = ["strawberries", "spinach", "kale", "nectarines", "apples","grapes", "peaches", "cherries", "pears", "tomatoes","celery", "potatoes"]
+fruit = ["strawberries", "nectarines", "peaches", "cherries", "grapes"]
+vegetables = ["spinach", "kale", "apples", "pears", "tomatoes", "celery", "potatoes"]
+dirty_dozen = fruit + vegetables
+print(dirty_dozen)
+dirty_dozen_1 = [fruit, vegetables]
+print(dirty_dozen_1)  # Prints the list of fruits and vegetables separately
---
+++ day-4/who will pay.py
@@ -0,0 +1,4 @@
+import random
+list = ["Soikot"]# "Jui", "Peal", "Mumu", "Monamee"]
+random_choice = random.choice(list)
+print(f"{random_choice} will pay the bill today!")  # Randomly selects a person to pay the bill
