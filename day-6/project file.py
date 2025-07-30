963f70a9520f8ce851f8cf907b30e05f0c8f8410
myshaafrin <myshaafrinjeba916@gmail.com>
2025-07-30 14:50:20 +0600

    Add new content

--- day-6/project file.py
+++ day-6/project file.py
@@ -1,1 +1,19 @@
+import random
+word_list = ['Soikot', 'camel', 'bungi', 'mango', 'banana']
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
+
+#Make the user guess a word
+guess = input(str("Guess a word:\n")).lower()
+
+
+print(guess)
+
+
+#Check if the guess word is right if right it will print write for each word
+for letter in chosen_word:
+    if chosen_word == guess:
+        print("Right")
+    else:
+        print("Wrong")
