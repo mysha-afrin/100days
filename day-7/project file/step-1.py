e1407201cb80e3f7c2dc829868cf88e1f0103a69
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-02 16:49:32 +0600

    Add new content

--- day-7/project file/step-1.py
+++ day-7/project file/step-1.py
@@ -2,13 +2,33 @@ import random
 word_list = ["banana", "apple", "reach", "terminal", "overtime"]
 chosen_word = random.choice(word_list)#chooses a word randomly.
 print(chosen_word)
-guess = input("Guess a word :")
-print(guess)
-place_holder = "_"
-word = (place_holder * len(chosen_word))
-print(word)
-for word in chosen_word:
-    if word in guess:
-        print("Right")
-    else:
-        print("Wrong")
+lives = 6
+place_holder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    place_holder += "_"
+print(place_holder)
+game_over = False
+correct_letter = []
+while not game_over:
+    guess = input("Guess a word:").lower()
+    #if guess in chosen_word:
+       # correct_letter.append(guess)
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
+            lives -= 1
+        print(display)
+
+if "_" not in display :
+    game_over = True
+    print("HUHU, you win!")
+else:
+    print("Oops, you lost!")
