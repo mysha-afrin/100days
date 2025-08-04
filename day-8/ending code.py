144e3b4a49d77abeedfc8f0c4034e90c2f0eb249
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-04 17:49:55 +0600

    Add new content

--- day-8/ending code.py
+++ day-8/ending code.py
@@ -1,1 +1,5 @@
-
+def great(name, country, profession):
+    print(f"My name is {name}.")
+    print(f"I was born in {country}.")
+    print(f"I am {profession}.")
+great("jeba", "Bangladesh", "hacker")
