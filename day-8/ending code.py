15e9abc3c4fd72855c85c6e515e75053e3b0e702
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-04 20:00:39 +0600

    Add new content

--- day-8/ending code.py
+++ day-8/ending code.py
@@ -3,3 +3,10 @@ def great(name, country, profession):
     print(f"I was born in {country}.")
     print(f"I am {profession}.")
 great("jeba", "Bangladesh", "hacker")
+
+
+
+def greating_people(name, country):
+    print(f"This is {name}.")
+    print(f"She is from {country}")
+greating_people(name = "Jeba", country= "Bangladesh")
