1c342a181529243420eaedb6fbe6de212e7d586b
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-06 21:28:03 +0600

    Add new content

--- day-8/ceasor.py
+++ day-8/ceasor.py
@@ -1,12 +1,13 @@
 alphabetes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k','l','m','n', 'o', 'p','q', 'r','s','t','u','v','w' ,'x','y','z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k','l','m','n', 'o', 'p','q', 'r','s','t','u','v','w' ,'x','y','z']
 direction = input("Type encode to encrypt and type decode to decrypt: \n")
 shift = int(input('Type the shift number \n'))
-if direction == "encode":
+'''if direction == "encode":
     text = ("Type your message : \n")
 elif direction == "decode":
     shifted_msg = input("Type your encoded message : \n")
 else:
-    print("You typed invalid.")
+    print("You typed invalid.")'''
+text = input("Type you message:")
 
 def encrypt(actual_message, shifted_amount):
     cyfer_text = ""
@@ -18,7 +19,7 @@ def encrypt(actual_message, shifted_amount):
     print(f"Here is the shifted message: {cyfer_text}")
 
     
-encrypt(actual_message= text, shifted_message= shift)
+encrypt(actual_message= text, shifted_amount= shift)
 
 
 
