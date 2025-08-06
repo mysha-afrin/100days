af0b69461c447cc9b28cb10d2a9a1ad67e95f5b4
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-07 00:28:07 +0600

    Add new content

--- day-8/ceasor.py
+++ day-8/ceasor.py
@@ -1,12 +1,8 @@
 alphabetes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k','l','m','n', 'o', 'p','q', 'r','s','t','u','v','w' ,'x','y','z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k','l','m','n', 'o', 'p','q', 'r','s','t','u','v','w' ,'x','y','z']
 direction = input("Type encode to encrypt and type decode to decrypt: \n")
 shift = int(input('Type the shift number \n'))
-'''if direction == "encode":
-    text = ("Type your message : \n")
-elif direction == "decode":
-    shifted_msg = input("Type your encoded message : \n")
-else:
-    print("You typed invalid.")'''
+
+
 text = input("Type you message:")
 
 def encrypt(actual_message, shifted_amount):
@@ -16,17 +12,29 @@ def encrypt(actual_message, shifted_amount):
     for letter in actual_message:
         shifted_position = alphabetes.index(letter) + shifted_amount
         cyfer_text += alphabetes[shifted_position]
+        shifted_position %= len(alphabetes)
+        cyfer_text += alphabetes[shifted_position]
     print(f"Here is the shifted message: {cyfer_text}")
-
-    
 encrypt(actual_message= text, shifted_amount= shift)
+def decrypt(original_text, shift_amount):
+    output_text = ""
 
+    for letter in original_text:
+        shifted_position = alphabetes.index(letter) - shift_amount
+        shifted_position %= len(alphabetes)
+        output_text += alphabetes[shifted_position]
+    print(f"Here is the shifted message: {output_text}")
+decrypt(original_text = text, shift_amount = shift)
+    
 
-
-
-
-
-
+def ceaser (original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+        shifted_position = alphabetes.index(letter) - shift_amount
+        shifted_position %= len(alphabetes)
+        output_text += alphabetes[shifted_position]
+    print(f"Here is the shifted message: {output_text}")
+ceaser(original_text = text, shift_amount = shift, encode_or_decode = direction)
 
 '''ask_user = input("Do you want to continue? y for yes and n for NO")
 if ask_user == "y":
