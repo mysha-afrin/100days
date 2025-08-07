5affbfc9f17e97eb12342414d7af37e6cb0b6fe5
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-07 21:50:29 +0600

    Add new content

--- day-8/ceasor.py
+++ day-8/ceasor.py
@@ -1,3 +1,7 @@
+from art import logo
+
+print(logo)
+
 alphabetes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k','l','m','n', 'o', 'p','q', 'r','s','t','u','v','w' ,'x','y','z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k','l','m','n', 'o', 'p','q', 'r','s','t','u','v','w' ,'x','y','z']
 direction = input("Type encode to encrypt and type decode to decrypt: \n")
 shift = int(input('Type the shift number \n'))
@@ -5,7 +9,7 @@ shift = int(input('Type the shift number \n'))
 
 text = input("Type you message:")
 
-def encrypt(actual_message, shifted_amount):
+'''def encrypt(actual_message, shifted_amount):
     cyfer_text = ""
 
 
@@ -24,17 +28,34 @@ def decrypt(original_text, shift_amount):
         shifted_position %= len(alphabetes)
         output_text += alphabetes[shifted_position]
     print(f"Here is the shifted message: {output_text}")
-decrypt(original_text = text, shift_amount = shift)
+decrypt(original_text = text, shift_amount = shift)'''
     
 
 def ceaser (original_text, shift_amount, encode_or_decode):
     output_text = ""
     for letter in original_text:
+        if letter not in alphabetes:
+            output_text += letter
+            continue
+        else:
+            if encode_or_decode == "decode":
+                shift_amount = -1
         shifted_position = alphabetes.index(letter) - shift_amount
         shifted_position %= len(alphabetes)
         output_text += alphabetes[shifted_position]
     print(f"Here is the shifted message: {output_text}")
 ceaser(original_text = text, shift_amount = shift, encode_or_decode = direction)
+
+
+should_continue = True
+while should_continue == True:
+    continue
+
+
+
+
+
+
 
 '''ask_user = input("Do you want to continue? y for yes and n for NO")
 if ask_user == "y":
