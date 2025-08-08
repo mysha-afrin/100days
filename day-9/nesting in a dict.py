ef96898012487580b1f8095562b9ccfd94f04519
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-09 00:05:48 +0600

    Add new content

---
+++ day-9/nesting in a dict.py
@@ -0,0 +1,8 @@
+capitals = {"Bangladesh": ["Dhaka", "Chittagong", "Khulna"],
+            "India": ["New Delhi", "Mumbai", "Kolkata"],
+            "Pakistan": ["Islamabad", "Karachi", "Lahore"]}
+
+print(capitals["Bangladesh"][0]) #Output: Dhaka
+
+nested_dict = ["a", "b", ["c", "d"]]
+print(nested_dict[2][1])
