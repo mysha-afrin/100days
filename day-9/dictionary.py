7f05b6bb58d260c500c0c58cf4f4c2b5f3032f72
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-08 23:26:45 +0600

    Add new content

--- day-9/dictionary.py
+++ day-9/dictionary.py
@@ -3,3 +3,8 @@ programming_dictionary = {"Bug": "An error in a program that prevents the progra
 
 
 print(programming_dictionary["Bug"])
+
+
+#Adding new items to the dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
