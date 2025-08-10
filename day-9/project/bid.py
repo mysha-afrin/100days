849175f8932ec8c581cfaedee243b498437a12cd
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-10 22:47:37 +0600

    Add new content

--- day-9/project/bid.py
+++ day-9/project/bid.py
@@ -1,2 +1,14 @@
 
+bid = {}
 
+
+continue_bidding = True
+while continue_bidding:
+    bidder = input("Enter your name: ")
+    money = int(input("Enter your bid amount: $"))
+    bid[bidder] = money
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no' :\n").lower()
+    if should_continue == "no":
+        continue_bidding = False
+    elif should_continue == "yes":
+        continue_bidding = True
