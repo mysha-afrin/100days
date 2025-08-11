90a39d4462cdd4dbbb5754b21cf9f99f28bad85f
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-11 22:45:57 +0600

    Add new content

--- day-9/project/bid.py
+++ day-9/project/bid.py
@@ -1,4 +1,5 @@
-
+from art import logo
+print(logo[0])
 bid = {}
 
 
@@ -12,3 +13,12 @@ while continue_bidding:
         continue_bidding = False
     elif should_continue == "yes":
         continue_bidding = True
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
