93264c0f4bb23d391d9945cfcdf727394f9641d1
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-14 20:18:17 +0600

    love you

--- day-11/casino game.py
+++ day-11/casino game.py
@@ -1,1 +1,13 @@
+import random
+
+from art import logo
+print(logo[0])
+
+print("Welcome to blackjack!")
+
+def deal_card():
+    """Function to deal two random cards."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
