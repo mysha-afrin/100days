2defdaa3755c660000867ea40d74c31c713d2522
myshaafrin <myshaafrinjeba916@gmail.com>
2025-08-30 17:46:14 +0600

    Add new content

--- day-15/main.py
+++ day-15/main.py
@@ -5,7 +5,8 @@ timmy.shape("turtle")
 timmy.color("coral")
 timmy.shape("turtle")
 timmy.color("aquamarine4")
-timmy.forward(100)
+timmy.forward(100)#move the turtle forward by 100 units
 my_screen = timmy.getscreen()
 print(my_screen.canvheight)
-my_screen.exitonclick()
+my_screen.exitonclick() #this line keeps the screen open untill we click on it.
+
