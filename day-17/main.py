41ee8c8cd756f2389e3f4f91ec389d7065ef52de
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-01 16:59:19 +0600

    Add new content

--- day-17/main.py
+++ day-17/main.py
@@ -1,1 +1,16 @@
 
+class User:
+    pass
+# pass is used as a placeholder for code that will be implemented later.
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "angela"
+user_1.password = "1234"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "jack"
+user_2.password = "abcd"
