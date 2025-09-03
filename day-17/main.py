d1d07324e8857ba95ad925dae51cf25a9b33f99d
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-03 14:30:01 +0600

    Add new content

--- day-17/main.py
+++ day-17/main.py
@@ -1,17 +1,21 @@
 
 class User:
-    def __init__(self, username, id, password):
+    def __init__(self, user_id, username):
+        self.user_id = user_id
         self.username = username
-        self.id = id
-        self.username = username
-        self.password = password
+        self.followers = 0
+        self.following = 0
+        
 # pass is used as a placeholder for code that will be implemented later.
-    def __repr__(self):
-        return f"User(username = {self.username}, id = {self.id}, password = {self.password})"
-    
+    #def __repr__(self):
+      #  return f"User(username = {self.username}, id = {self.id}, password = {self.password})"
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 #create user objects
-user_1 = User("Angela", 1, "password123")
-user_2 = User("Jack", 2, "qwerty")
+user_1 = User("Angela", 1)
+user_2 = User("Jack", 2)
+user_1.follow(user_2)
 #Print user objects
-print(user_1)
-print(user_2)
+print(user_1.followers)
+print(user_2.followers)
