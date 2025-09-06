9e4dfe930363ed6458a5ffb48f2fabe17a332f11
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-06 14:11:13 +0600

    love you

--- day-17/quiz_brain.py
+++ day-17/quiz_brain.py
@@ -6,3 +6,4 @@ class QuizBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)? ")
+        print(user_answer)
