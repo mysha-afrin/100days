a91b1caccca7559ca04564b5847068077d0ac2d4
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-09 14:54:16 +0600

    love you

--- day-18/main.py
+++ day-18/main.py
@@ -1,12 +1,10 @@
 from turtle import Turtle, Screen
-
-
+import random
 timmy_the_turtle = Turtle()
+timmy_the_turtle.color("red")
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("DarkOrchid4")
-for _ in range(8):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
-
-screen = Screen()
+for _ in range(4):
+   
+    timmy_the_turtle.forward(10)
+screen = Screen
 screen.exitonclick()
