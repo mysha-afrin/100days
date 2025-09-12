9dd1df7e1865383243d026ccf75fd86450c38845
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-12 22:55:51 +0600

    love you

--- day-18/drawing random turtle.py
+++ day-18/drawing random turtle.py
@@ -2,21 +2,28 @@ import turtle as t
 from turtle import Screen
 import random
 
+screen = Screen()
+screen.colormode(255)
 
 tim = t.Turtle()
+#colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+
+tim.pensize(15)
 tim.speed("fastest")
-tim.pensize(10)
 
-
-colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"]
 directions = [0, 90, 180, 270]
 
-
 for _ in range(200):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    tim.color(r, g, b)
     tim.forward(30)
-    tim.color(random.choice(colors))
     tim.setheading(random.choice(directions))
     
 
-screen = Screen()
+
 screen.exitonclick()
+
+#tim --- IGNORE ---
--- day-18/main_1.py
+++ day-18/main_1.py
@@ -1,13 +1,19 @@
 import turtle as t
-from turtle import Screen
+from turtle import Screen, colormode 
 import random
 tim = t.Turtle()
-
+tim.pensize(5)
+#colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 for num_sides in range(3, 10):
     angle = 360 / num_sides
     for _ in range(num_sides):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
         tim.forward(100)
         tim.right(angle)
+        
 
 screen = Screen()
 screen.exitonclick()
+screen.colormode(255)
