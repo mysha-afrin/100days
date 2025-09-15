c8e1bd413df4330e0c905f35b9dc0b93076ba0f0
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-15 22:22:18 +0600

    love you

--- day-19/main.py
+++ day-19/main.py
@@ -1,1 +1,15 @@
+from turtle import Turtle, Screen
 
+tim = Turtle()
+
+def move_forwards():
+    tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+
+
+screen = Screen()
+screen.listen()
+screen.onkey(key = "space", fun = move_forwards)
+screen.onkey(key = "b", fun = move_backwards)
+screen.exitonclick()
