3c38a6cb8ca3f489fed6dec000e1ee26222c80ce
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-16 15:32:40 +0600

    love you

--- day-19/turtle_race.py
+++ day-19/turtle_race.py
@@ -1,36 +1,38 @@
 from turtle import Turtle, Screen
 import random
-
-tim = Turtle()
-timmy = Turtle()
-tommy = Turtle()
-teddy = Turtle()
-tina = Turtle()
-toby = Turtle()
-
-colors = ["red", "blue", "green", "yellow", "purple", "orange"]
-tim.color(random.choice(colors))
-timmy.color(random.choice(colors))
-tommy.color(random.choice(colors))
-teddy.color(random.choice(colors))
-tina.color(random.choice(colors))
-toby.color(random.choice(colors))
-
-
-tim.shape("turtle")
-timmy.shape("turtle")
-tommy.shape("turtle")
-teddy.shape("turtle")
-tina.shape("turtle")
-toby.shape("turtle")
-
-
-
-
-
-
-
-
 screen = Screen()
+screen.setup(width = 500, height = 400)
+user_bet = screen.textinput(title = "Make your bet", prompt = "Which turtle will win the race? Enter a color:")
+y_position = [-70, -40, -10, 20, 50, 80]
+tim = Turtle()
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+for turtle_index in range(0, 6):
+    tim = Turtle(shape = "turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x = -200, y = y_position[turtle_index])
+    
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    rand_distance = random.randint(0, 10)
+    turtle.forward(rand_distance)
+
+
+
+#colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+#tim.color(random.choice(colors))
+
+
+
+
+
+
+
+
+
+
+
 
 screen.exitonclick()
