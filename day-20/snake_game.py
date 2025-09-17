cd254275ec00093231b8b7c6ba21f1b4f4d8ddfe
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-17 15:43:42 +0600

    love you

--- day-20/snake_game.py
+++ day-20/snake_game.py
@@ -1,1 +1,33 @@
+from turtle import Turtle, Screen
+import time
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+#snake.shape("square")
 
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+    new_segment.penup()
+    segments.append(new_segment)
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
+    
+
+
+
+
+
+screen.mainloop()
