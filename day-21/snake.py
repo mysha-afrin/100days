217fe2ee60b3eed1b917197ae7eb2b325d8303bd
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-19 15:28:16 +0600

    love you

---
+++ day-21/snake.py
@@ -0,0 +1,54 @@
+import turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+    def create_snake(self):
+       
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+    def add_segment(self, position):
+        new_segment = turtle.Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+    def move(self):
+        for seg_num in range(len(self.segments) -1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+screen = turtle.Screen()
+screen.setup(width = 600, height= 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+snake = Snake()
+screen.listen()
+screen.onkey(key = "Up", fun = snake.up)
+screen.onkey(key = "Down", fun = snake.down)
+screen.onkey(key = "Left", fun = snake.left)
+screen.onkey(key = "Right", fun = snake.right)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    turtle.time.sleep(0.1)
+    snake.move()
+screen.exitonclick()
