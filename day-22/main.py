58ffa537e158b96a4a0646042f747c0c1277827f
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-25 21:24:29 +0600

    LOVE YOU

--- day-22/main.py
+++ day-22/main.py
@@ -1,1 +1,30 @@
 
+import random
+from turtle import Turtle, Screen
+import game_essentials
+import time
+from game_essentials import Player
+
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.title("Pong")
+screen.bgcolor("black")
+screen.tracer(0)
+#player = Player()
+
+l_paddle = game_essentials.l_paddle
+r_paddle = game_essentials.r_paddle
+
+screen.listen()
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+screen.exitonclick()
