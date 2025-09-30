d8ecc13e5720464fe1456e47f4b1fe7bb8aba1d1
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-30 23:05:38 +0600

    love you

---
+++ day-23/car_manager.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+import random
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        all_cars = []
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_wid = 2, stretch_len= 1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
