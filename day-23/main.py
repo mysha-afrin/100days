6db18405cc9897c29fb8c1d4d25d31ce682d09ec
myshaafrin <myshaafrinjeba916@gmail.com>
2025-09-30 22:41:49 +0600

    love you

--- day-23/main.py
+++ day-23/main.py
@@ -1,1 +1,14 @@
+from turtle import Screen
+from player import Player
+import time
 
+screen = Screen()
+screen.setup(width=800, height=800)
+screen.tracer()
+
+player = Player()
+
+game_is_on = True
+while game_is_on :
+    time.sleep(.1)
+    screen.update()
