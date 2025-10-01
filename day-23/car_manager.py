e43cad1b8298837db4f710c259e128d8823f3c0e
myshaafrin <myshaafrinjeba916@gmail.com>
2025-10-01 23:32:06 +0600

    love you

--- day-23/car_manager.py
+++ day-23/car_manager.py
@@ -7,12 +7,17 @@ MOVE_INCREMENT = 10
 
 class CarManager:
     def __init__(self):
-        all_cars = []
+        self.all_cars = []
     def create_car(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid = 2, stretch_len= 1)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))
-        random_y = random.randint(-250, 250)
-        random_x = random.randint(-250, 250)
-        
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid = 1, stretch_len= 2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+    def move_cars(self):
+        for cars in self.all_cars:
+            cars.backward(STARTING_MOVE_DISTANCE)
--- day-23/main.py
+++ day-23/main.py
@@ -1,14 +1,25 @@
 from turtle import Screen
 from player import Player
 import time
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(width=800, height=800)
-screen.tracer()
+screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 game_is_on = True
 while game_is_on :
     time.sleep(.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+    #Detect car collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+
+screen.exitonclick()
