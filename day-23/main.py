da9a014a8e55622f74514414dcbb6430e3eab950
myshaafrin <myshaafrinjeba916@gmail.com>
2025-10-03 16:41:43 +0600

    love you

--- day-23/main.py
+++ day-23/main.py
@@ -2,14 +2,14 @@ from turtle import Screen
 from player import Player
 import time
 from car_manager import CarManager
-
+from score_board import ScoreBoard
 screen = Screen()
 screen.setup(width=800, height=800)
 screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
-
+score_board = ScoreBoard()
 screen.listen()
 screen.onkey(player.go_up, "Up")
 #screen.onkey(player.go_down, "Down")
@@ -23,9 +23,12 @@ while game_is_on :
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            score_board.game_over()
     #detect successfull crossing
     if player.finish_line():
         player.go_to_starting_position()
         car_manager.level_up()
+        score_board.increase_level()
+        score_board.update_score_board()
 
 screen.exitonclick()
