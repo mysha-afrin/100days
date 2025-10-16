7f269882877f1fb2f79d6470b187039eee6064e5
myshaafrin <myshaafrinjeba916@gmail.com>
2025-10-16 23:39:48 +0600

    Love you

--- day-27/main.py
+++ day-27/main.py
@@ -4,8 +4,9 @@ import tkinter
 window = tkinter.Tk()
 window.title("My First GUI Program")
 
-
-
+window.minsize(width = 500, height = 300)
+my_lebel = tkinter.Label(text = "I am a Label")
+my_lebel.pack(side = "left")
 
 
 window.mainloop()
