c6958d8b911faaed9b51200a67b6bd5a77fb3da3
myshaafrin <myshaafrinjeba916@gmail.com>
2025-10-19 15:35:13 +0600

    Love you

--- day-27/playground.py
+++ day-27/playground.py
@@ -4,3 +4,13 @@ def add(*args):
         sum += num
     return sum
 print(add(5, 10, 15, 20))
+
+
+
+class car:
+    def __init__ (self, **kwargs):
+        print(kwargs)
+        n += kwargs["add"]
+        n *= kwargs["multiply"]
+        print(n)
+car(2, add = 3, multiply = 5)
