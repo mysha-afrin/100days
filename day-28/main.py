9e2a0e36e25f4df11beabdcac8c53136da9ac8df
myshaafrin <myshaafrinjeba916@gmail.com>
2025-12-21 06:33:24 +0600

    love you

--- day-28/main.py
+++ day-28/main.py
@@ -54,7 +54,49 @@ canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0)
 canvas.grid(column=2, row=2)
 tomato_img = PhotoImage(file = Path(sys.path[0]) / "tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
+
+
+timer_text = canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.config(text = f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = windows.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text = marks)
+
+
+
+
+
+
+
+def start_timer():
+    global reps
+    reps += 1 
+    work_min = WORK_MIN * 60
+    short_break_min = SHORT_BREAK_MIN * 60
+    long_break_min = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        count_down(long_break_min)
+        title_label.config(text = "Break", fg = RED)
+    elif reps % 2 == 0:
+        count_down(short_break_min)
+        title_label.config(text = "Break", fg = PINK)
+    else:
+        count_down(work_min)
+        title_label.config(text = "Work", fg = GREEN)
 
 
 
