3a139a8a3e355a1a0666cbc2c9e1d437e4efb554
myshaafrin <myshaafrinjeba916@gmail.com>
2026-01-12 12:38:58 +0600

    Love You

--- day-30/main.py
+++ day-30/main.py
@@ -10,13 +10,13 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-try:
+def generate_phonetic():
     word = input("Enter a word: ").upper()
-    output_list = [phonetic_dict[letter] for letter in word]
-    print(output_list)
-except KeyError:
-    word = input("please enter only letters from A to Z:").upper()
-    output_list = [phonetic_dict[letter] for letter in word]
-    print(output_list)
-except KeyError as error_message:
-    print(f"The letter {error_message} is not in the alphabet.")
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+generate_phonetic()
