33161180238b05b3d52d93a591a25daa694027d5
myshaafrin <myshaafrinjeba916@gmail.com>
2026-01-28 20:45:35 +0600

    love you

--- day-32/main.py
+++ day-32/main.py
@@ -1,53 +1,38 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
 import smtplib
 import random
 import datetime as dt
+import pandas
 
-now =dt.datetime.now()
-
-days_of_week = now.weekday()
-print(days_of_week)
-
-
+now = dt.datetime.now()
+current_day = now.day
+print(f"current day is {current_day}")
 my_email = "ahonarahman2026@gmail.com"
 password = "kuedtaitjjarlfet"  
 
-
-if days_of_week == 5:
-    with open("day-32/birthday.txt", "r") as file:
-        print("opening file")
-        quote_lines = file.readlines()
-   # print(quote_lines)
-if not quote_lines:
-    print("file is empty")
-else:
-    random_quote = random.choice(quote_lines)
-    print(random_quote)
-    print('randomly_choosen')
-    print("Step 1: creating SMTP object")
+with open("day-32/birthdays.csv") as file:
+    data = pandas.read_csv(file)
+    birthdays_dict = data.to_dict(orient="records")
+    print("step 1: birthdays dict created")
+if current_day == birthdays_dict[2]["day"]:
+    print("birthday match found")
+    with open("day-32/letter1.txt") as letter_file:
+        letter_contents1 = letter_file.read()
+        print("step2 :read letter 1")
+    with open("day-32/letter2.txt") as letter_file:
+        letter_contents2 = letter_file.read()
+        print("reade letter 2")
+    with open("day-32/letter3.txt") as letter_file:
+        letter_contents3 = letter_file.read()
+        print("read letter 3")
+    letter_content = random.choice([letter_contents1, letter_contents2, letter_contents3])
+    letter_sent_to = birthdays_dict[1]["name"]
+    print("step 3: letter content chosen")
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        print("Step 2: connected to server")
-
         connection.starttls()
-        print("Step 3: TLS started")
         connection.login(my_email, password)
-        print("Step 4: logged in")
+        print("step 4: logged in to email server")
         connection.sendmail(from_addr=my_email,
-                            to_addrs="myshaafrinjeba916@gmail.com",
-                            msg=f"Subject:Friday Motivation\n\n{random_quote}"
+                                to_addrs="myshaafrinjeba916@gmail.com",
+                                msg=f"Subject:Friday Motivation\n\n{letter_content}"
                             )
-        print("Step 5: mail sent")
-
-        
-        print("Step 6: connection closed")
-
+        print("step 5: email sent")
