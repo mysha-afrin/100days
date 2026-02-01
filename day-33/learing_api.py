d35d6c8096dfaa8e9d6a6e5937e8e49d5ca8e3e7
myshaafrin <myshaafrinjeba916@gmail.com>
2026-02-01 16:18:52 +0600

    love you

--- day-33/learing_api.py
+++ day-33/learing_api.py
@@ -1,4 +1,7 @@
 import requests
+import json
 
-response = requests.get(url = "http://open-notify.org/Open-Notify-API/ISS-Location-Now/")
-print(response)
+response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json() ["iss_position"]
+print(data)
