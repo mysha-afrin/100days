4d46134f0bab3190bb6a28a9908a5c77a601afee
myshaafrin <myshaafrinjeba916@gmail.com>
2026-02-03 15:46:11 +0600

    love you

--- day-33/learing_api.py
+++ day-33/learing_api.py
@@ -1,7 +1,25 @@
 import requests
 import json
 
-response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+MY_LAT = 22.845640
+MY_LONG = 89.540329
+
+
+'''response = requests.get(url = "http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data = response.json() ["iss_position"]
-print(data)
+print(data)'''
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG
+}
+
+response = requests.get(url = "https://api.sunrise-sunset.org/json" , params = parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunrise)
+print(sunset)
