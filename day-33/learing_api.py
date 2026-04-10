676b0ea392c6dd084822dfb5204c8c794419ee1c
myshaafrin <myshaafrinjeba916@gmail.com>
2026-04-10 22:09:13 +0600

    love you

--- day-33/learing_api.py
+++ day-33/learing_api.py
@@ -1,35 +1,4 @@
 import requests
-import json
-from datetime import datetime, timezone
-MY_LAT = 22.845640
-MY_LONG = 89.540329
 
-
-'''response = requests.get(url = "http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json() ["iss_position"]
-print(data)'''
-
-
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0
-}
-
-response = requests.get(url = "https://api.sunrise-sunset.org/json" , params = parameters)
-response.raise_for_status()
-data = response.json()
-#sunrise = data["results"]["sunrise"]
-#sunset = data["results"]["sunset"]
-#print(sunrise)
-#print(sunset)
-
-
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
-print(sunrise.split("T")[1].split(":")[0])
-
-
-time_now = datetime.now()
-print(time_now.hour)
+response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+print(response.status_code)
--- day-33/salami.py/main.py
+++
@@ -1,7 +0,0 @@
-import random
-
-admins = ["entropy", "tanvir", "sumaiya", "sadik", "light", "samir"]
-
-admin_salami = random.choice(admins)
-
-print(f" Ajke salami paben {admin_salami}  ")
