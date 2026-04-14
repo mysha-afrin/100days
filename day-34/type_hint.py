72ab127272f7f32dd16afd272e73b79c8aaf6685
myshaafrin <myshaafrinjeba916@gmail.com>
2026-04-14 15:53:14 +0600

    love you

---
+++ day-34/type_hint.py
@@ -0,0 +1,3 @@
+def greeting(name: str) -> str:
+    #first str denotes the type of the parameter and the second str denotes the return type of the function
+    return "Hello" + name 
--- day-34/ui.py
+++ day-34/ui.py
@@ -1,9 +1,10 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self, quiz_brain):
+    def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
@@ -11,12 +12,26 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.canvas.grid(row = 1 , column=0, columnspan=2, pady = 20)
         self.question_text = self.canvas.create_text(150, 125, width=280, text="Some Question Text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
-        true_image = PhotoImage(file="day-34/true.png")
-        false_image = PhotoImage(file="day-34/false.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_image = PhotoImage(file="day-34/true.png")
+        self.false_image = PhotoImage(file="day-34/false.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
         self.score_label = Label(text="Score: 0", bg = THEME_COLOR, fg= "white")
         self.score_label.grid(row=0, column=2)
+        self.get_next_question()
         self.window.mainloop()
+
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text = q_text)
+
+    def true_pressed(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+
+    def false_pressed(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()
