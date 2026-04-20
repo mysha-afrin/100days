aa9d15c73fe279e837b826631a46c137bd1854d9
myshaafrin <myshaafrinjeba916@gmail.com>
2026-04-20 09:28:00 +0600

    love you

--- day-35/main.py
+++ day-35/main.py
@@ -1,10 +1,13 @@
 import requests
 
-
+parameters = { "days" : 1,
+              "hours" : 3
+              }
 
 url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/bagerhat%2Cbangladesh?unitGroup=us&key=U8G3LPY4MZKJ8NTZRWK5P8CKC&contentType=json"
 
-
-response = requests.get(url)
+response = requests.get(url, params=parameters)
+response.raise_for_status()
 data = response.json()
-print(data)
+returned_data = data["days"][0]
+print(returned_data['sunrise'])
