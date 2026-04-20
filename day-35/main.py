5a5da1e0d762d5ff264fb179b76bb7fbfc1c5eab
myshaafrin <myshaafrinjeba916@gmail.com>
2026-04-20 22:30:45 +0600

    love you

--- day-35/main.py
+++ day-35/main.py
@@ -1,4 +1,5 @@
 import requests
+import smtplib
 
 parameters = { "days" : 1,
               "hours" : 3
@@ -9,5 +10,24 @@ url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/servic
 response = requests.get(url, params=parameters)
 response.raise_for_status()
 data = response.json()
-returned_data = data["days"][0]
-print(returned_data['sunrise'])
+returned_data = data["days"][0]["hours"][0]
+def check_rain():
+    for hour in data["days"][0]["hours"][::3]:
+        if hour["conditions"] == "Rain" or hour["conditions"] == "Thunderstorm" or hour["conditions"] == "Partially cloudy":
+            print(f"At {hour['datetime']}, it's {hour['conditions']}. Take a umbrella")
+        else:
+            print(f"At {hour['datetime']}, it's {hour['conditions']}. No rain, enjoy your day")
+
+my_email = "ahonarahman2026@gmail.com"
+password = "kuedtaitjjarlfet" 
+send_to_email = "myshaafrinjeba916@gmail.com"       
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        print("step 4: logged in to email server")
+        connection.sendmail(from_addr=my_email,
+                                to_addrs= send_to_email,
+                                msg=f"Subject:Weather Update\n\n{check_rain()}"
+                            )
+        print("step 5: email sent")
