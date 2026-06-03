dee9e57cf15a51fc4d3eda07a1bf0fe97bf7e181
myshaafrin <myshaafrinjeba916@gmail.com>
2026-06-03 19:25:27 +0600

    love you

--- day-38/main.py
+++ day-38/main.py
@@ -1,16 +1,2 @@
 import requests
 from datetime import datetime
-USERNAME = "your_username"
-
-
-#personal details
-GENDER = "female"
-WEIGHT_KG = 65
-HEIGHT_CM = 160
-AGE = 21
-
-exercise_text = input("Tell me which exercises you did:")
-
-# Nutritionix API credentials
-APP_ID  = "your_app_id"
-API_KEY = "your_api_key"
